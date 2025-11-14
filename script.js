7686b36a58c6e10a4458a137f5756d0312ba7fed
Himanshu Singh <himanshusingh1k@gmail.com>
2025-11-14 11:58:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-//your code here!
-const ordList = document.getElementById("infi-list");
-let itemCount = 1;
-for (let i = 0; i < 10; i++) {
-  const listItem = document.createElement("li");
-  listItem.setAttribute("class", `list-item-${itemCount}`);
-  listItem.innerText = `Item ${itemCount}`;
-  ordList.append(listItem);
+const ol = document.querySelector("#infi-list");
+function listCreation() {
+  const li = document.createElement("li");
+  li.setAttribute("class", `list-item-${itemCount}`);
+  li.innerText = `Item ${itemCount}`;
+  ol.append(li);
   itemCount++;
 }
-function loadMoreContent() {
-  for (let i = 0; i < 2; i++) {
-    const listItem = document.createElement("li");
-    listItem.setAttribute("class", `list-item-${itemCount}`);
-    listItem.innerText = `Item ${itemCount}`;
-    ordList.append(listItem);
-    itemCount++;
+let itemCount = 1;
+for (let i = 0; i < 10; i++) listCreation();
+ol.addEventListener("scroll", () => {
+  if (ol.scrollTop + ol.clientHeight >= ol.scrollHeight - 10) {
+    for (let i = 0; i < 2; i++) listCreation();
   }
-}
-ordList.addEventListener("scroll", () => {
-  if (ordList.scrollTop + ordList.clientHeight >= ordList.scrollHeight - 10)
-    loadMoreContent();
 });
